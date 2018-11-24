74b53eb346965dc2490546b58eb8532f0b6901d2
shiqi.li <1009903985@qq.com>
2018-11-24 20:09:57 +0800

    test ant design

--- render/views/Page2/index.js
+++ render/views/Page2/index.js
@@ -1,12 +1,75 @@
 import React, { Component } from 'react';
-import { observer } from 'mobx-react';
+import { Table, Icon } from 'antd';
 
-@observer
+const { Column, ColumnGroup } = Table;
+
+
+const data = [{
+  key: '1',
+  firstName: 'John',
+  lastName: 'Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+}, {
+  key: '2',
+  firstName: 'Jim',
+  lastName: 'Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+}, {
+  key: '3',
+  firstName: 'Joe',
+  lastName: 'Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+}];
+
 class Page2 extends Component {
 
   render() {
     return (
-      <h2>第二页</h2>
+      <div style={{ padding: '50px' }}>
+        <h2>ant design</h2>
+        <Table dataSource={data}>
+          <ColumnGroup title="Name">
+            <Column
+              title="First Name"
+              dataIndex="firstName"
+              key="firstName"
+            />
+            <Column
+              title="Last Name"
+              dataIndex="lastName"
+              key="lastName"
+            />
+          </ColumnGroup>
+          <Column
+            title="Age"
+            dataIndex="age"
+            key="age"
+          />
+          <Column
+            title="Address"
+            dataIndex="address"
+            key="address"
+          />
+          <Column
+            title="Action"
+            key="action"
+            render={(text, record) => (
+              <span>
+                <a href="#">Action 一 {record.name}</a>
+                <span className="ant-divider" />
+                <a href="#">Delete</a>
+                <span className="ant-divider" />
+                <a href="#" className="ant-dropdown-link">
+                  More actions <Icon type="down" />
+                </a>
+              </span>
+            )}
+          />
+        </Table>
+      </div>
     );
   }
 }
