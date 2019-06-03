2a89203610630d51f7e76367662fb1d39a02384b
shiqi.li <1009903985@qq.com>
2019-06-03 20:04:08 +0800

    :bug: fix

--- render/views/Window/index.js
+++ render/views/Window/index.js
@@ -34,7 +34,7 @@ class Window extends React.Component {
       <div className={styles.demoContainer}>
         <Alert className={styles.margin} message="点击打开一个普通窗口" type="info" />
         <Button className={styles.margin} onClick={this.createWindow}>打开窗口</Button>
-        <Alert className={styles.margin} message="点击打开一个无框窗口(点击下面【关闭所有窗口】即可关闭)" type="success" />
+        <Alert className={styles.margin} message="点击打开一个无框窗口" type="success" />
         <Button className={styles.margin} onClick={this.createWNoBarWindow}>打开窗口</Button>
         <Alert className={styles.margin} message="点击打开一个带有状态栏的无框窗口" type="warning" />
         <Button className={styles.margin} onClick={this.createNoBarWindowWithButton}>打开窗口</Button>
